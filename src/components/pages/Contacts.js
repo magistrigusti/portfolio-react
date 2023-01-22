2e3f719933a202cb2efaa8d5b688af81d76eb26f
Magistry <Magistrigusti@yandex.ru>
2023-01-22 05:08:52 +0200

    0.05(contacts)

--- src/components/pages/Contacts.js
+++ src/components/pages/Contacts.js
@@ -7,15 +7,15 @@ const Contacts = () => {
         <ul className="content-list">
           <li className="content-list__item">
             <h2 className="title-2">Location</h2>
-            <p>Moscow, Russia</p>
+            <p>Holon, Izrael</p>
           </li>
           <li className="content-list__item">
             <h2 className="title-2">Telegram / WhatsApp</h2>
-            <p><a href="tel:+79051234567">+7 (905) 123-45-67</a></p>
+            <p><a href="tel:+79051234567">+972 52-310-0674</a></p>
           </li>
           <li className="content-list__item">
             <h2 className="title-2">Email</h2>
-            <p><a href="mailto:webdev@protonmail.com">webdev@protonmail.com</a></p>
+            <p><a href="mailto:webdev@protonmail.com">magistrigusti@gmail.com</a></p>
           </li>
         </ul>
 
